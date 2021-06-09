68894d66152004c3e444bb11dcc0142178f3710b
sixblades <khalilovaha@gmail.com>
2021-06-09 19:59:36 +0300

    Twitter Monitor Added

---
+++ twitter-monitor/src/main/java/io/enigmasolutions/twittermonitor/TwitterMonitorApplication.java
@@ -0,0 +1,13 @@
+package io.enigmasolutions.twittermonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterMonitorApplication.class, args);
+    }
+
+}
---
+++ twitter-monitor/src/test/java/io/enigmasolutions/twittermonitor/TwitterMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.enigmasolutions.twittermonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
