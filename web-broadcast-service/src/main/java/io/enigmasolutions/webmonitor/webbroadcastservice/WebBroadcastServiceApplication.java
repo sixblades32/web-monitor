61ba76838206b390725c4159f76a795b2136ca56
darthpresident <dartherpresdent@gmail.com>
2021-07-21 01:26:44 +0300

    Update configs, prepare Web Broadcast for prod

--- web-broadcast-service/src/main/java/io/enigmasolutions/webmonitor/webbroadcastservice/WebBroadcastServiceApplication.java
+++ web-broadcast-service/src/main/java/io/enigmasolutions/webmonitor/webbroadcastservice/WebBroadcastServiceApplication.java
@@ -2,10 +2,8 @@ package io.enigmasolutions.webmonitor.webbroadcastservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class WebBroadcastServiceApplication {
 
     public static void main(String[] args) {
