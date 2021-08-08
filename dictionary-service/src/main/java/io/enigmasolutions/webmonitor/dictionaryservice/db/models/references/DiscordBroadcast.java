b6cfb57d2fca8f2e6336d1edaf1105cea59c69fd
dartherpresdent@gmail.com <3jg@kb@TBU$VcXx>
2021-08-08 23:40:49 +0300

    Update models

--- dictionary-service/src/main/java/io/enigmasolutions/webmonitor/dictionaryservice/db/models/references/DiscordBroadcast.java
+++ dictionary-service/src/main/java/io/enigmasolutions/webmonitor/dictionaryservice/db/models/references/DiscordBroadcast.java
@@ -1,10 +1,16 @@
 package io.enigmasolutions.webmonitor.dictionaryservice.db.models.references;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DiscordBroadcast {
     private List<String> baseWebhooks;
     private List<String> liveWebhooks;
