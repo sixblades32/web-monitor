59ca6b1be1b9b65c3abc4613d15f917890da8db5
dartherpresdent@gmail.com <3jg@kb@TBU$VcXx>
2021-08-08 15:43:37 +0300

    Update pord configs for Dictionary and Auth services

--- dictionary-service/src/test/java/io/enigmasolutions/webmonitor/dictionaryservice/DictionaryServiceApplicationTests.java
+++ dictionary-service/src/test/java/io/enigmasolutions/webmonitor/dictionaryservice/DictionaryServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package io.enigmasolutions.webmonitor.dictionaryservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DictionaryServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package io.enigmasolutions.webmonitor.dictionaryservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DictionaryServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
