08ee9ff2b572a5529bacb581eb9561017ae04138
dartherpresdent@gmail.com <3jg@kb@TBU$VcXx>
2021-08-08 22:43:47 +0300

    Enable scheduling

--- discord-broadcast-service/src/main/java/io/enigmarobotics/discordbroadcastservice/DiscordBroadcastServiceApplication.java
+++ discord-broadcast-service/src/main/java/io/enigmarobotics/discordbroadcastservice/DiscordBroadcastServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = CustomerRepository.class)
 @EnableCaching
+@EnableScheduling
 public class DiscordBroadcastServiceApplication {
 
     public static void main(String[] args) {
