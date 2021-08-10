1591ab978b526dca4e4a9075ec40a621ae4add34
dartherpresdent@gmail.com <3jg@kb@TBU$VcXx>
2021-08-11 00:40:45 +0300

    Update documents

--- twitter-monitor/src/main/java/io/enigmasolutions/twittermonitor/db/models/documents/TwitterConsumer.java
+++ twitter-monitor/src/main/java/io/enigmasolutions/twittermonitor/db/models/documents/TwitterConsumer.java
@@ -1,11 +1,17 @@
 package io.enigmasolutions.twittermonitor.db.models.documents;
 
 import io.enigmasolutions.twittermonitor.db.models.references.Credentials;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "twitter-consumers")
 public class TwitterConsumer {
     @Id
--- twitter-monitor/src/main/java/io/enigmasolutions/twittermonitor/db/models/documents/TwitterScraper.java
+++ twitter-monitor/src/main/java/io/enigmasolutions/twittermonitor/db/models/documents/TwitterScraper.java
@@ -3,11 +3,17 @@ package io.enigmasolutions.twittermonitor.db.models.documents;
 import io.enigmasolutions.twittermonitor.db.models.references.Credentials;
 import io.enigmasolutions.twittermonitor.db.models.references.TweetDeckAuth;
 import io.enigmasolutions.twittermonitor.db.models.references.TwitterUser;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "twitter-scrapers")
 public class TwitterScraper {
     @Id
