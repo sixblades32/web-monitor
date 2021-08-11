f6c50e768870870115bb7fef499427134bb36b5d
dartherpresdent@gmail.com <3jg@kb@TBU$VcXx>
2021-08-11 21:28:25 +0300

    Fix alert bug

--- broadcast-models/src/main/java/io/enigmasolutions/broadcastmodels/Alert.java
+++ broadcast-models/src/main/java/io/enigmasolutions/broadcastmodels/Alert.java
@@ -3,10 +3,12 @@ package io.enigmasolutions.broadcastmodels;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Alert {
     private Integer validMonitorsCount;
     private Integer failedMonitorsCount;
