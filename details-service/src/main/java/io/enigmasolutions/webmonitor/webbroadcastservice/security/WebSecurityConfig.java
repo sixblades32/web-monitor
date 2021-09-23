fba67ab7176c78b8a52518e0718fc9f0d3e8bb08
dartherpresdent@gmail.com <3jg@kb@TBU$VcXx>
2021-09-24 00:00:04 +0300

    CORS config

--- details-service/src/main/java/io/enigmasolutions/webmonitor/webbroadcastservice/security/WebSecurityConfig.java
+++ details-service/src/main/java/io/enigmasolutions/webmonitor/webbroadcastservice/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package io.enigmasolutions.webmonitor.webbroadcastservice.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -8,9 +9,12 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
+@Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class WebSecurityConfig {
@@ -52,17 +56,13 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public CorsWebFilter corsConfiguration() {
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsWebFilter(source);
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
