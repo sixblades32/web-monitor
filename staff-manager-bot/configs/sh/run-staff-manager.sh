1a12c102cb4b449f7a23b8ebd3263ff91103f83b
dartherpresdent@gmail.com <3jg@kb@TBU$VcXx>
2021-10-26 21:34:24 +0300

    Change configs

--- staff-manager-bot/configs/sh/run-staff-manager.sh
+++ staff-manager-bot/configs/sh/run-staff-manager.sh
@@ -5,5 +5,8 @@ gradle clean build -p web-monitor/staff-manager
 export DISCORD_BOT_TOKEN=
 export DICTIONARY_SERVICE_URL=http://localhost:8015/api/dictionary
 export KAFKA_BOOTSTRAP_SERVERS=localhost:9092
+export TWITTER_MONITOR_URL=http://localhost:8030/twitter-monitor
+export MONITOR_USER_NAME=admin
+export MONITOR_USER_PASSWORD=admin
 
-nohup java -jar web-monitor/twitter-monitor/build/libs/staff-manager*.jar &
+nohup java -jar web-monitor/staff-manager-bot/build/libs/staff-manager-bot*.jar &
