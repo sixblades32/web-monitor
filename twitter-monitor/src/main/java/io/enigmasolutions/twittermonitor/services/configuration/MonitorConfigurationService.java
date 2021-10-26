a5ffbcd96660332588ca70c269a7e2e2b59fd75f
sixblades <khalilovaha@gmail.com>
2021-10-26 22:09:01 +0300

    Config

--- twitter-monitor/src/main/java/io/enigmasolutions/twittermonitor/services/configuration/MonitorConfigurationService.java
+++ twitter-monitor/src/main/java/io/enigmasolutions/twittermonitor/services/configuration/MonitorConfigurationService.java
@@ -19,11 +19,13 @@ import io.enigmasolutions.twittermonitor.services.monitoring.TwitterHelperServic
 import java.util.List;
 
 import io.enigmasolutions.twittermonitor.services.web.DictionaryClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 public class MonitorConfigurationService {
 
   private final TwitterHelperService twitterHelperService;
@@ -188,6 +190,7 @@ public class MonitorConfigurationService {
     try{
       user = twitterHelperService.retrieveUser(followRequest.getTwitterUser().getLogin());
     }catch (Exception e){
+      log.error(e.getMessage());
       throw new NoTwitterUserMatchesException();
     }
 
